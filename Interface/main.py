9671b55266a3f2a49e1d6e1c5431f1e18d2e2fff
Khusnullin Michael <53513063+GradientFox@users.noreply.github.com>
2024-11-02 16:13:04 +0500

    Upd Latest ver

--- Interface/main.py
+++ Interface/main.py
@@ -1,9 +1,30 @@
-from PyQt6.QtGui import QIcon, QPixmap
-from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt5.QtCore import QSize
+from PyQt5.QtGui import QIcon, QPalette, QColor
+from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QVBoxLayout, QLabel
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Python")
+        self.setBaseSize(QSize(800, 800))
+        wd = QWidget(self)
+        wd.setFocus()
+        l = QVBoxLayout(wd)
+        sc = QLabel()
+        sr = QLabel()
+        sc.setBaseSize(QSize(100, 100))
+        sr.setBaseSize(QSize(200, 150))
+        pal1 = QPalette()
+        pal1.setColor(QPalette.Background, QColor('red'))
+        sc.setPalette(pal1)
+        l.addWidget(sc)
+        l.addWidget(sr)
+        wd.setLayout(l)
+        self.setCentralWidget(wd)
+
 
 app = QApplication([])
-app.setApplicationName("Hello, world!")
-window = QWidget()
-
+window = MainWindow()
 window.show()
 app.exec()
